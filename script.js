c1373e291a7c5a0e7d653efad6b4fcf191d6a740
onthewaysia <atsqw001@gmail.com>
2022-09-12 17:53:13 +0800

    timer done

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 //Game notification
+const gameNoti = document.querySelector(".game-notification");
+let gameState = "loading";
+
+function delay(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+async function startGame() {
+  await delay(750);
+  gameNoti.innerText = "Get ready!";
+  await delay(1000);
+  gameNoti.innerText = "3";
+  await delay(1000);
+  gameNoti.innerText = "2";
+  await delay(1000);
+  gameNoti.innerText = "1";
+  await delay(1000);
+  gameNoti.innerText = "Go!";
+  gameState = "running";
+  await delay(1000);
+  gameNoti.innerText = "Hold Space to move!";
+  start();
+  countdown();
+}
+
+startGame();
+
+//Timer
+let timer = 10;
+const time = document.querySelector(".game-timer");
+
+async function countdown() {
+  while (timer > 0) {
+    await delay(1000);
+    timer--;
+    time.innerText = timer;
+  }
+}
 
 //Doll movement
 function turnForward() {
@@ -25,8 +63,6 @@ async function start() {
   this.start();
 }
 
-start();
-
 //Character movement
 const character = document.getElementById("player");
 let currentLocation = "5px";
@@ -36,15 +72,17 @@ let foot = "left";
 window.addEventListener("keydown", function (e) {
   const locale = player.style.left;
   if (e.code == "Space") {
-    currentLocation = parseFloat(currentLocation) + step;
-    character.style.left = `${currentLocation}px`;
-    console.log(character.src);
-    if (foot == "left") {
-      character.src = "./images/character_right.png";
-      foot = "right";
-    } else {
-      character.src = "./images/character_left.png";
-      foot = "left";
+    if (gameState == "running") {
+      currentLocation = parseFloat(currentLocation) + step;
+      character.style.left = `${currentLocation}px`;
+      console.log(character.src);
+      if (foot == "left") {
+        character.src = "./images/character_right.png";
+        foot = "right";
+      } else {
+        character.src = "./images/character_left.png";
+        foot = "left";
+      }
     }
   }
 });
